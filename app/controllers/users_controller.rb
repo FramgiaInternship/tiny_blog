ce838eeda9d40b95421e5c48f018b4f90398f4b0
Son Tran <sontd.it@gmail.com>
2016-10-17 12:38:44 +0700

    Finish signup account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   attr_accessor :name, :email
   def new
+    @btn_submit = t "users.new.btn_submit"
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "users.new.msg_success"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
 
   get '/home', to: 'static_pages#home', as: 'home'
 
   get '/help', to: 'static_pages#help', as: 'help'
 
+  resources :users
+  get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
 end
