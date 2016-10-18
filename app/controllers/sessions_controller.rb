0b159b537afc376de7f0f1b08300cf7ad30a6b21
Son Tran <sontd.it@gmail.com>
2016-10-18 21:08:06 +0700

    Implements remember login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_back_or root_url
     else
       flash.now[:danger] = t "sessions.log_in.log_in_fails"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_many :entries,  dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :active_relationships,  class_name: :Relationship,
@@ -19,6 +20,19 @@ class User < ApplicationRecord
   has_secure_password
   before_save { email.downcase! }
 
+  class << self
+    # Returns the hash digest of the given string.
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
   # Follows a user.
   def follow other_user
     active_relationships.create followed_id: other_user.id
@@ -32,5 +46,22 @@ class User < ApplicationRecord
   # Returns true if the current user is following the other user.
   def following? other_user
     following.include? other_user
+  end
+
+  # Remembers a user in the database for use in peristent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  # Return true if the given token matches the digest.
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(self.remember_digest).is_password? remember_token
   end
 end
